7b60030eadd53c9424d682d1d74baf6166548726
Frank Niessink <frank@niessink.com>
2005-07-19 21:56:42 +0000

    Added clock.

---
+++ taskcoachlib/date/clock.py
@@ -0,0 +1,33 @@
+import patterns, wx, time
+
+class Clock(patterns.Observable):
+    __metaclass__ = patterns.Singleton
+    
+    def __init__(self, *args, **kwargs):
+        super(Clock, self).__init__(*args, **kwargs)
+        self._timer = wx.PyTimer(self.startTheClock)
+        now = time.time()
+        millisecondsToNextWholeSecond = 1000-(now%1)*1000
+        self._timer.Start(milliseconds=millisecondsToNextWholeSecond, oneShot=True)
+        
+    def startTheClock(self, *args, **kwargs):
+        self.notify()
+        self._clock = wx.PyTimer(self.notify)
+        self._clock.Start(milliseconds=1000, oneShot=False)
+        
+    def notify(self, *args, **kwargs):
+        self.notifyObservers(patterns.Notification(source=self))
+        
+        
+class ClockObserver(object):    
+    def startClock(self):
+        Clock().registerObserver(self.onEverySecond)
+        
+    def stopClock(self):
+        Clock().removeObserver(self.onEverySecond)
+
+    def isClockStarted(self):
+        return self.onEverySecond in Clock().observers()
+        
+    def everySecond(self, *args, **kwargs):
+        raise NotImplementedError
