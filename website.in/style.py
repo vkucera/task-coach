b70732f033b77f57c4b59d512185d3edea83d27b
Frank Niessink <frank@niessink.com>
2005-10-30 16:31:43 +0000

    Automatic commit due to green-bar

--- website.in/style.py
+++ website.in/style.py
@@ -2,66 +2,89 @@ from taskcoachlib import meta
 
 
 header = '''
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<HTML>
-    <HEAD>
-        <META http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD html 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
         <style type="text/css" media="screen">@import "default.css";</style>
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-        <TITLE>%(name)s</TITLE>
-    </HEAD>
-    <BODY>
-        <DIV class="content">
-            <H1><IMG align="center" src="taskcoach.png"/>&nbsp;%(name)s - %(description)s</H1>
-        </DIV>
-        <DIV class="content">
+        <title>%(name)s</title>
+    </head>
+    <body>
+        <div class="content">
+            <h1>
+                <img align="center" src="taskcoach.png"/>
+                &nbsp;%(name)s - %(description)s
+            </h1>
+        </div>
+        <div class="content">
 '''%meta.metaDict
 
 footer = '''        
-        </DIV><!-- end of content DIV -->
-        <DIV id="navAlpha">
-            <DIV class="navbox">
-                <H2>Contents</H2>
-                <P>
-                    <A HREF="index.html">Home</A><BR>
-                    <A HREF="https://sourceforge.net/projects/taskcoach/">Sourceforge
-                    home</A><BR>
-                    <A HREF="download.html" title="Get %(name)s here">Download</A><BR>
-                    <A HREF="screenshots.html">Screenshots</A><BR>
-                    <A HREF="features.html" title="List of features in the current version of %(name)s">Features</A><BR>
-                    <A HREF="i18n.html">Translations</A><BR>
-                    <A HREF="https://sourceforge.net/tracker/?group_id=130831&atid=719134">Known bugs</A><BR>
-                    <A HREF="changes.html">Change history</A><BR>
-                    <A HREF="license.html">License</A><BR>
-                    <A HREF="credits.html">Credits</A>
-                </P>
-            </DIV>
-            <DIV class="navbox">
+        </div><!-- end of content div -->
+        <div id="navAlpha">
+            <div class="navbox">
+                <h2>Contents</h2>
+                <p>
+                    <a href="index.html" title="%(name)s homepage">Home</a><br>
+                    <a href="https://sourceforge.net/projects/taskcoach/"
+                       title="%(name)s at Sourceforge">Sourceforge home</a><br>
+                    <a href="download.html" 
+                       title="Get %(name)s here">Download</a><br>
+                    <a href="screenshots.html" 
+                       title="View some screenshots of %(name)s here">
+                         Screenshots</a><br>
+                    <a href="features.html" 
+                       title="List of features in the current version of %(name)s">Features</a><br>
+                    <a href="i18n.html">Translations</a><br>
+                    <a href="https://sourceforge.net/tracker/?group_id=130831&atid=719134">Known bugs</a><br>
+                    <a href="changes.html">Change history</a><br>
+                    <a href="roadmap.html" 
+                       title="Future plans for %(name)s">Roadmap</a><br>
+                    <a href="license.html">License</a><br>
+                    <a href="credits.html">Credits</a>
+                </p>
+            </div>
+            <div class="navbox">
                 <h2>Support</h2>
-                <P>
-                    <A HREF="mailinglist.html">Join mailinglist</A><BR>
-                    <A HREF="faq.html">Frequently asked questions</A><BR>
-                    <A HREF="https://sourceforge.net/tracker/?group_id=130831&atid=719137">Request a feature</A><BR>
-                    <A HREF="https://sourceforge.net/tracker/?func=add&group_id=130831&atid=719134">Submit a bug report</A>
-                </P>
-            </DIV>
-        </DIV>
-        <DIV id="navBeta">
-            <DIV class="navbox">
-                <H2>Links</H2>
-                <P>
-                    <A HREF="http://www.python.org">Python</A><BR>
-                    <A HREF="http://www.wxpython.org">wxPython</A><BR>
-                    <A HREF="http://www.icon-king.com">Nuvola icon set</A><BR>
-                    <A HREF="http://www.jrsoftware.org">Inno Setup</A><BR>
-                    <A HREF="http://www.bluerobot.com">Bluerobot.com</A><BR>
-                    <A href="http://sourceforge.net"><IMG
+                <p>
+                    <a href="mailinglist.html">Join mailinglist</a><br>
+                    <a href="faq.html">Frequently asked questions</a><br>
+                    <a href="https://sourceforge.net/tracker/?group_id=130831&atid=719137">Request a feature</a><br>
+                    <a href="https://sourceforge.net/tracker/?func=add&group_id=130831&atid=719134">Submit a bug report</a>
+                </p>
+            </div>
+        </div>
+        <div id="navBeta">
+            <div class="navbox">
+                <h2>Links</h2>
+                <p>
+                    <a href="http://www.python.org">Python</a><br>
+                    <a href="http://www.wxpython.org">wxPython</a><br>
+                    <a href="http://www.icon-king.com">Nuvola icon set</a><br>
+                    <a href="http://www.jrsoftware.org">Inno Setup</a><br>
+                    <a href="http://www.bluerobot.com">Bluerobot.com</a><br>
+                    <a href="http://sourceforge.net"><img
                 src="http://sourceforge.net/sflogo.php?group_id=130831&type=1"
-                width="88" height="31" border="0" alt="SourceForge.net Logo"/></A>
-                </P>
-            </DIV>
-        </DIV>
-    </BODY>
-</HTML>
+                width="88" height="31" border="0" alt="SourceForge.net Logo"/></a>
+                </p>
+            </div>
+            <div class="navbox">
+                <h2>A word of warning</h2>
+                <p>
+                    %(name)s is currently alpha-state software. 
+                    New releases are made available approximately 
+                    twice a month and old releases are not supported.
+                    There are no separate stable and development branches, 
+                    just a development branch. New versions usually contain 
+                    a mix of new or changed features and bugfixes, and 
+                    unfortunately, sometimes new bugs. So, if you use 
+                    %(name)s in a production-like setting, backing up your 
+                    work on a regular basis is strongly advised.
+                </p>
+            </div>
+        </div>
+    </body>
+</html>
 '''%meta.metaDict
 
