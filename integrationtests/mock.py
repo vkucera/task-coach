bc82f3a22a5e86b273747c2adda69001d069e0fa
Frank Niessink <frank@niessink.com>
2006-03-26 18:51:54 +0000

    A mock taskcoach application object to be reused in the integration tests.

---
+++ integrationtests/mock.py
@@ -0,0 +1,28 @@
+import taskcoach
+import domain.task as task
+
+class MockWxApp(object):
+    def SetAppName(self, *args, **kwargs):
+        pass
+
+    def SetVendorName(self, *args, **kwargs):
+        pass
+
+    
+class App(taskcoach.App):
+    def __init__(self, args=None):
+        self._options = None
+        self._args = args or []
+        self.wxApp = MockWxApp()
+        self.init()
+
+    def init(self):
+        super(App, self).init(loadSettings=False)
+
+    def addTasks(self):
+        self.parent = task.Task()
+        self.child = task.Task()
+        self.parent.addChild(self.child)
+        self.taskFile.extend([self.parent])
+
+
