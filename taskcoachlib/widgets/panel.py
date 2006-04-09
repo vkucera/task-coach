4f16c07623b71aad2bfc0a9c45ff1a19a11c5df1
Frank Niessink <frank@niessink.com>
2006-04-09 09:31:26 +0000

    In the dateTimeCtrl: make timeCtrl disabled too, when dateCtrl is disabled.

--- taskcoachlib/widgets/panel.py
+++ taskcoachlib/widgets/panel.py
@@ -18,19 +18,22 @@ class PanelWithBoxSizer(wx.Panel):
         
 class BoxWithFlexGridSizer(wx.Panel):
     ''' A panel that is boxed and has a FlexGridSizer inside it. '''
-    def __init__(self, parent, label, cols, gap=10, vgap=0, hgap=0, growableRow=-1, growableCol=-1, *args, **kwargs):
+    def __init__(self, parent, label, cols, gap=10, vgap=0, hgap=0, 
+            growableRow=-1, growableCol=-1, *args, **kwargs):
         super(BoxWithFlexGridSizer, self).__init__(parent, *args, **kwargs)
         box = wx.StaticBox(self, label=label)
         self.__boxSizer = wx.StaticBoxSizer(box, wx.HORIZONTAL)
-        self.__entriesSizer = wx.FlexGridSizer(cols=cols, vgap=gap or vgap, hgap=gap or hgap)
+        self.__entriesSizer = wx.FlexGridSizer(cols=cols, vgap=gap or vgap, 
+            hgap=gap or hgap)
         if growableRow > -1:
             self.__entriesSizer.AddGrowableRow(growableRow, proportion=1)
         if growableCol > -1:
             self.__entriesSizer.AddGrowableCol(growableCol, proportion=1)
-        self.__boxSizer.Add(self.__entriesSizer, proportion=1, flag=wx.EXPAND|wx.ALL, border=10)
+        self.__boxSizer.Add(self.__entriesSizer, proportion=1, 
+            flag=wx.EXPAND|wx.ALL, border=10)
         
     def fit(self):
-        ''' Call this method after all controls have been added (via Add()). '''
+        ''' Call this method after all controls have been added (via add()). '''
         self.SetSizerAndFit(self.__boxSizer)
         
     def add(self, control, *args, **kwargs):
