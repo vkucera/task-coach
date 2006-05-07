249480789f784f647c2c2e83dd2af06ba485f76d
Frank Niessink <frank@niessink.com>
2006-05-07 16:22:46 +0000

    Automatic commit due to green-bar

--- taskcoach/unittests/thirdpartyTests/wxPythonTest.py
+++ taskcoach/unittests/thirdpartyTests/wxPythonTest.py
@@ -1,15 +1,20 @@
 import test, wx
 
+''' These are unittests of wxPython functionality. Of course, the goal is
+    not to test all wxPython functions, but rather to document platform
+    inconsistencies or surpiring behaviour. '''
+
+
 class TextCtrlTest(test.wxTestCase):
-    def testClearDoesNotEmitEventOnMacOSX(self):
+    def testCleaEmitsEventOnMSWOnly(self):
         self.clearTextCausesEvent = False
         textCtrl = wx.TextCtrl(self.frame)
         textCtrl.Bind(wx.EVT_TEXT, self.onTextChanged)
         textCtrl.Clear()
-        if '__WXMAC__' in wx.PlatformInfo:
-            self.failIf(self.clearTextCausesEvent)
-        else:
+        if '__WXMSW__' in wx.PlatformInfo:
             self.failUnless(self.clearTextCausesEvent)
+        else:
+            self.failIf(self.clearTextCausesEvent)
 
     def onTextChanged(self, event):
         self.clearTextCausesEvent = True
