d1c22a6b9268050f81d1076eda5696ad25947b5e
Frank Niessink <frank@niessink.com>
2006-07-07 22:59:21 +0000

    Fixed bug with composite efforts.

--- taskcoachlib/domain/effort/reducer.py
+++ taskcoachlib/domain/effort/reducer.py
@@ -1,10 +1,12 @@
 import patterns, effort
+from domain import date
 
 
 class EffortAggregator(patterns.ListDecorator):
     ''' This class observes an TaskList and aggregates the individual effort
         records to CompositeEfforts, e.g. per day or per week. This class is 
-        abstract. Subclasses should implement timePeriod(effort).'''
+        abstract. Subclasses should implement startOfPeriod(dateTime)
+        and endOfPeriod(dateTime). '''
 
     def __init__(self, *args, **kwargs):
         self.__taskAndTimeToCompositesMap = {}
@@ -67,8 +69,9 @@ class EffortAggregator(patterns.ListDecorator):
         parent = event.source()
         child = event.value()
         for effort in child.efforts(recursive=True):
-            newComposites.extend(self._addEffortToCompositeForTask(effort,
-                parent))
+            for ancestor in child.ancestors():
+                newComposites.extend(self._addEffortToCompositeForTask(effort,
+                    ancestor))
         super(EffortAggregator, self).extendSelf(newComposites)
 
     def onChildRemovedFromTask(self, event):
@@ -76,8 +79,9 @@ class EffortAggregator(patterns.ListDecorator):
         parent = event.source()
         child = event.value()
         for effort in child.efforts(recursive=True):
-            compositesToRemove.extend( \
-                self._removeEffortFromCompositeForTask(effort, parent))
+            for ancestor in child.ancestors():
+                compositesToRemove.extend( \
+                    self._removeEffortFromCompositeForTask(effort, ancestor))
         super(EffortAggregator, self).removeItemsFromSelf(compositesToRemove)
 
     def _addEfforts(self, efforts):
@@ -92,7 +96,8 @@ class EffortAggregator(patterns.ListDecorator):
 
     def _addEffortToCompositeForTask(self, newEffort, task):
         newComposites = []
-        timePeriod = self.timePeriod(newEffort)
+        timePeriod = self.startOfPeriod(newEffort.getStart()), \
+                     self.endOfPeriod(newEffort.getStart())
         key = (task.id(), timePeriod)
         composite = self.__taskAndTimeToCompositesMap.setdefault(key,
             effort.CompositeEffort(task, *timePeriod))
@@ -122,7 +127,14 @@ class EffortAggregator(patterns.ListDecorator):
                     compositesToRemove.append(composite)
         return compositesToRemove
 
-    def timePeriod(self, effort):
+    def startOfPeriod(self, dateTime):
+        ''' For a given dateTime, startOfPeriod returns the start-dateTime 
+            of the period dateTime belongs to. '''
+        raise NotImplementedError
+
+    def endOfPeriod(self, dateTime):
+        ''' For a given dateTime, endOfPeriod returns the end-dateTime 
+            of the period dateTime belongs to. '''
         raise NotImplementedError
 
     def originalLength(self):
@@ -131,83 +143,18 @@ class EffortAggregator(patterns.ListDecorator):
             effort records.'''
         return len(self)
 
-
-
-"""
-class _EffortAggregator(patterns.ListDecorator):
-    ''' This class observes an EffortList and aggregates the individual effort
-        records to CompositeEfforts, e.g. per day or per week. This class is 
-        abstract. Subclasses should implement timePeriod(effort).'''
-    
-    def __init__(self, *args, **kwargs):
-        self.__taskAndTimeToCompositesMap = {}
-        self.__effortToCompositeMap = {}
-        super(EffortAggregator, self).__init__(*args, **kwargs)
-        
-    def extendSelf(self, efforts):
-        ''' extendSelf is called when an item is added to the observed
-            list. The default behavior of extendSelf is to add the item
-            to the observing list (i.e. this list) unchanged. We override 
-            the default behavior to first group the effort by time period
-            and then add the CompositeEffort(s) to this list. '''
-        newComposites = []
-        for newEffort in efforts:
-            newEffort.registerObserver(self.onEffortChanged, 'effort.start',
-                'effort.task')
-            for task in [newEffort.task()] + newEffort.task().ancestors():
-                timePeriod = self.timePeriod(newEffort)
-                key = (task.id(), timePeriod)
-                composite = self.__taskAndTimeToCompositesMap.setdefault(key,
-                    effort.CompositeEffort(task, *timePeriod))
-                if len(composite) == 0:
-                    newComposites.append(composite)
-                composite.append(newEffort)
-                self.__effortToCompositeMap.setdefault(newEffort, 
-                    []).append(composite)
-        super(EffortAggregator, self).extendSelf(newComposites)
-
-    def removeItemsFromSelf(self, efforts):
-        ''' removeItemsFromSelf is called when an item is removed from the 
-            observed list. The default behavior of removeItemsFromSelf is to 
-            remove the item from the observing list (i.e. this list)
-            unchanged. We override the default behavior to remove the 
-            effort from the CompositeEffort is it part of. '''
-        compositesToRemove = []
-        for effort in efforts:
-            effort.removeObserver(self.onEffortChanged)
-            composites = self.__effortToCompositeMap.pop(effort)
-            for composite in composites:
-                composite.remove(effort)
-                if len(composite) == 0:
-                    compositesToRemove.append(composite)
-        super(EffortAggregator, self).removeItemsFromSelf(compositesToRemove)
-
-    def onEffortChanged(self, event):
-        effortChanged = event.source()
-        self.removeItemsFromSelf([effortChanged])
-        self.extendSelf([effortChanged])
-
-    def timePeriod(self, effort):
-        raise NotImplementedError
-
-    def originalLength(self):
-        ''' Do not delegate originalLength to the observed TaskList because 
-            that would return a number of tasks, and not the number of 
-            effort records.'''
-        return len(self)
-"""
 
 class EffortPerDay(EffortAggregator):        
-    def timePeriod(self, effort):
-        return effort.getStart().startOfDay(), effort.getStart().endOfDay()
+    startOfPeriod = date.DateTime.startOfDay
+    endOfPeriod = date.DateTime.endOfDay
 
 
 class EffortPerWeek(EffortAggregator): 
-    def timePeriod(self, effort):
-        return effort.getStart().startOfWeek(), effort.getStart().endOfWeek()
+    startOfPeriod = date.DateTime.startOfWeek
+    endOfPeriod = date.DateTime.endOfWeek
 
 
 class EffortPerMonth(EffortAggregator):
-    def timePeriod(self, effort):
-        return effort.getStart().startOfMonth(), effort.getStart().endOfMonth()
+    startOfPeriod = date.DateTime.startOfMonth
+    endOfPeriod = date.DateTime.endOfMonth
         
