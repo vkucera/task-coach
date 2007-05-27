3ebe837b24349e1be29531bb6b67362860fba08d
Frank Niessink <frank@niessink.com>
2007-05-27 22:34:46 +0000

    Fixed typo in comment. Refactored to remove duplication and split up __init__ to better show what is going on.

--- taskcoach/taskcoachlib/i18n/__init__.py
+++ taskcoach/taskcoachlib/i18n/__init__.py
@@ -4,25 +4,24 @@ class Translator:
     __metaclass__ = patterns.Singleton
     
     def __init__(self, languageAndCountry=None):
-        if not languageAndCountry:
-            return
+        if languageAndCountry:
+            language = languageAndCountry[:2] # e.g. 'nl_NL'[:2] == 'nl'
+            self._importTranslation(language)
+            self._setLocale(languageAndCountry, language)
 
-        language = languageAndCountry[:2]
-
-        # Import translation dicstionary
+    def _importTranslation(self, language):
         if language != 'en': 
             module = __import__(language, globals())
             self.__language = module.dict
             self.__encoding = module.encoding
 
-        # Try to find language info 
-        for localeString in languageAndCountry, language:
-            li = wx.Locale.FindLanguageInfo(localeString)
-            if li:
-                self.__locale = wx.Locale(li.Language)
+    def _setLocale(self, *localeStrings):
+        for localeString in localeStrings:
+            languageInfo = wx.Locale.FindLanguageInfo(localeString)
+            if languageInfo:
+                self.__locale = wx.Locale(languageInfo.Language)
                 break
 
-         
     def translate(self, string):
         try:
             return self.__language[string].decode(self.__encoding)
