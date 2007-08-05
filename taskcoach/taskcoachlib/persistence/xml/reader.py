e5d4fc029725d0fc2cc6661d0cdcaef3b9ea1ae2
Jérôme Laheurte <jerome@jeromelaheurte.net>
2007-08-05 10:53:35 +0000

    Save attachments alongside taskcoach file

--- taskcoach/taskcoachlib/persistence/xml/reader.py
+++ taskcoach/taskcoachlib/persistence/xml/reader.py
@@ -1,9 +1,17 @@
-import time, xml.dom.minidom, re
+import time, xml.dom.minidom, re, os
 from domain import date, effort, task, category, note, attachment
 
 class XMLReader:
     def __init__(self, fd):
         self.__fd = fd
+
+        # Determine where are attachments.
+        path, name = os.path.split(os.path.abspath(self.__fd.name))
+        name, ext = os.path.splitext(name)
+        attdir = os.path.normpath(os.path.join(path, name + '_attachments'))
+        if not os.path.exists(attdir):
+            os.mkdir(attdir)
+        attachment.MailAttachment.attdir = attdir
 
     def read(self):
         domDocument = xml.dom.minidom.parse(self.__fd)
@@ -23,6 +31,7 @@ class XMLReader:
             notes = []
         else:
             notes = self.__parseNoteNodes(domDocument.documentElement.childNodes)
+
         return tasks, categories, notes
 
     def __parseTskVersionNumber(self, domDocument):
