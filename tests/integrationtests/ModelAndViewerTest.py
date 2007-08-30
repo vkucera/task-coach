7e9feff34973a8d0696e70430aeca0ae98f54f5c
Frank Niessink <frank@niessink.com>
2007-08-30 19:33:40 +0000

    Make integration tests run without exceptions.

--- tests/integrationtests/ModelAndViewerTest.py
+++ tests/integrationtests/ModelAndViewerTest.py
@@ -1,46 +1,33 @@
-import test, config, gui
-from domain import task, effort, category, note
+import test, mock
+from domain import task, category
 
 
 class TaskViewerAndCategoryFilterIntegrationTestFixture(test.wxTestCase):
     def setUp(self):
-        self.settings = config.Settings(load=False)
-        self.taskList = task.TaskList()
-        self.categories = category.CategoryList()
-        self.category = category.Category('category')
-        self.categories.append(self.category)
-        self.notes = note.NoteContainer()
-        self.settings.set('view', 'sortby', 'subject')
-        viewerContainer = gui.viewercontainer.ViewerContainer(self.frame, 
-            self.settings, 'mainviewer')
-        self.viewer = self.TaskViewerClass(self.frame, self.taskList, 
-            gui.uicommand.UICommands(self.frame, None, viewerContainer, 
-            self.settings, 
-            self.taskList, effort.EffortList(self.taskList), self.categories,
-            self.notes), 
-            self.settings, categories=self.categories)
+        self.app = mock.App()
         parent = task.Task('parent')
         child = task.Task('child')
         parent.addChild(child)
-        self.taskList.append(parent)
+        self.category = category.Category('category')
+        self.app.mainwindow.taskFile.categories().append(self.category)
+        self.app.mainwindow.taskFile.tasks().append(parent)
         self.category.addTask(child)
         self.category.setFiltered()
+        
         
 
 class TaskListViewerAndCategoryFilterIntegrationTest( \
         TaskViewerAndCategoryFilterIntegrationTestFixture):
-    TaskViewerClass = gui.viewer.TaskListViewer
             
     def testFilterOnCategoryChildDoesHideParent(self):
-        self.assertEqual(1, self.viewer.widget.GetItemCount())
+        self.assertEqual(1, self.app.mainwindow.viewer[0].widget.GetItemCount())
 
 
 class TaskTreeListViewerAndCategoryFilterIntegrationTest( \
         TaskViewerAndCategoryFilterIntegrationTestFixture):
-    TaskViewerClass = gui.viewer.TaskTreeListViewer
             
     def testFilterOnCategoryChildDoesNotHideParent(self):
-        self.viewer.expandAll()
-        self.assertEqual(2, self.viewer.widget.GetItemCount())
+        self.app.mainwindow.viewer[1].expandAll()
+        self.assertEqual(2, self.app.mainwindow.viewer[1].widget.GetItemCount())
         
     
