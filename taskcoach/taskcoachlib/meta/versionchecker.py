b798af52aa9c2028727997397375e5d30a0cab29
Frank Niessink <frank@niessink.com>
2007-09-02 19:26:07 +0000

    Added a copy of the ElementTree package to the Task Coach source code, so the source code distribution of Task Coach works with Python 2.4, without needing to install ElementTree.

--- taskcoach/taskcoachlib/meta/versionchecker.py
+++ taskcoach/taskcoachlib/meta/versionchecker.py
@@ -1,10 +1,18 @@
 import threading, wx, meta, urllib2
 from i18n import _
 import gui.dialog.version
+
 # We don't use cElementTree because py2exe somehow does not
 # include cElementTree. ElementTree is no problem though...
-import xml.etree.ElementTree 
-    
+# We also can't use cElementTree (easily) because it's not part of the
+# Python 2.4 module library, so if import of ElementTree fails we get our
+# own copy from the thirdparty package.
+try:
+    import xml.etree.ElementTree as ElementTree
+except ImportError:
+    import thirdparty.ElementTree as ElementTree
+   
+   
 class VersionChecker(threading.Thread):
     def __init__(self, settings):
         self.settings = settings
@@ -22,7 +30,7 @@ class VersionChecker(threading.Thread):
             padFile = self.retrievePadFile()
         except urllib2.URLError:
             return self.settings.get('version', 'notified')
-        pad = xml.etree.ElementTree.parse(padFile)
+        pad = ElementTree.parse(padFile)
         return pad.findtext('Program_Info/Program_Version')
     
     def retrievePadFile(self):
