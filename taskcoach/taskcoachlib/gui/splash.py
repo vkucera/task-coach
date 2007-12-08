5e399a315d73f6699c78d729b4ad443276939f6d
Frank Niessink <frank@niessink.com>
2007-12-08 20:36:56 +0000

    Never mirror the splash screen, not even when using a RTL language.

--- taskcoach/taskcoachlib/gui/splash.py
+++ taskcoach/taskcoachlib/gui/splash.py
@@ -1,7 +1,15 @@
-import wx, icons
+import wx, icons, i18n
 
 class SplashScreen(wx.SplashScreen):
     def __init__(self):
-        super(SplashScreen, self).__init__( 
-            icons.catalog['splash'].getBitmap(),
+        splash = icons.catalog['splash']
+        if i18n.Translator().currentLanguageIsRightToLeft():
+            # RTL languages cause the bitmap to be mirrored too, but because
+            # the splash image is not internationalized, we have to mirror it
+            # (back). Unfortunately using SetLayoutDirection() on the 
+            # SplashWindow doesn't work.
+            bitmap = wx.BitmapFromImage(splash.getImage().Mirror())
+        else:
+            bitmap = splash.getBitmap()
+        super(SplashScreen, self).__init__(bitmap,
             wx.SPLASH_CENTRE_ON_SCREEN|wx.SPLASH_TIMEOUT, 4000, None, -1)
