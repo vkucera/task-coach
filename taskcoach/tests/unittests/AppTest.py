c3d90d7cbc5ac18616ba48e44c343ec92e5e73d3
Frank Niessink <frank@niessink.com>
2008-05-03 19:30:24 +0000

    Make sure all tests pass...

--- taskcoach/tests/unittests/AppTest.py
+++ taskcoach/tests/unittests/AppTest.py
@@ -24,7 +24,7 @@ class AppTests(test.TestCase):
     def testAppProperties(self):
         # Normally I prefer one assert per test, but creating the app is
         # expensive, so we do all the queries in one test method.
-        app = application.App(loadSettings=False, loadTaskFile=False)
+        app = application.Application(loadSettings=False, loadTaskFile=False)
         wxApp = wx.GetApp()
         self.assertEqual(meta.name, wxApp.GetAppName())
         self.assertEqual(meta.author, wxApp.GetVendorName())
