981daaa39ec8a1f3dd1b842239cb76329988ac45
Frank Niessink <frank@niessink.com>
2008-06-28 20:19:05 +0000

    Remove link to Sourceforge 'nominate best project' page.

--- website.in/style.py
+++ website.in/style.py
@@ -81,8 +81,7 @@ footer = '''
             </div>
             <div class="navbox">
                 <h2>Give support</h2>
-                <p>
-                    <a href="http://sourceforge.net/awards/cca/?project_name=TaskCoach&project_url=https://sourceforge.net/projects/taskcoach/"><img src="http://sourceforge.net/awards/cca/badge_img.php?project_name=TaskCoach&project_url=https://sourceforge.net/projects/taskcoach/&style=4" border="0"/></a><br>
+                <p>                    
                     <a href="i18n.html">Help translate</a><br>
                     <a href="devinfo.html">Help develop</a><br>
                     <a href="donations.html">Donate</a><br>
