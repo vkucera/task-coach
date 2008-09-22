d64e8a36f0878d5d31e66c685d3281f6c5fafba4
Jérôme Laheurte <jerome@jeromelaheurte.net>
2008-09-22 13:12:33 +0000

    Remove unused import

--- taskcoachlib/domain/attachment/attachment.py
+++ taskcoachlib/domain/attachment/attachment.py
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-import os, shutil, tempfile
+import os, shutil
 
 from taskcoachlib.domain import base
 from taskcoachlib.thirdparty import desktop
