7a5581327de159144523f5bc78f8b7f8e6744fae
Jérôme Laheurte <jerome@jeromelaheurte.net>
2009-01-25 12:53:09 +0000

    Show number of tasks in each category

--- taskcoach.sql
+++ taskcoach.sql
@@ -53,6 +53,7 @@ CREATE UNIQUE INDEX idxMetaName ON Meta (name);
 
 -- Views
 
+CREATE VIEW AllTask AS SELECT * FROM Task WHERE status != 3 ORDER BY name;
 CREATE VIEW OverdueTask AS SELECT * FROM Task WHERE status != 3 AND dueDate < DATE('now') ORDER BY dueDate, startDate DESC;
 CREATE VIEW DueTodayTask AS SELECT * FROM Task WHERE status != 3 AND dueDate == DATE('now') ORDER BY startDate DESC;
 CREATE VIEW StartedTask AS SELECT * FROM Task WHERE status != 3 AND startDate IS NOT NULL AND startDate <= DATE('now') AND (dueDate > DATE('now') OR dueDate IS NULL) ORDER BY startDate;
