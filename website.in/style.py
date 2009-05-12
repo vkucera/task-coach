03fe443a8eabf5eecd9eadcb7b254416461a793d
Frank Niessink <frank@niessink.com>
2009-05-12 21:11:55 +0000

    Added Google Analytics to website.

--- website.in/style.py
+++ website.in/style.py
@@ -136,6 +136,15 @@ src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
                 <script type="text/javascript" src="http://twitter.com/statuses/user_timeline/taskcoach.json?callback=twitterCallback2&amp;count=3"></script>
             </div>
         </div>
+        <script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-8814256-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
     </body>
 </html>
 '''%meta.metaDict
