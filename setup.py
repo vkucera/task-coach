b72103f203c87cc4014b6e8b709e6f13bf56a59c
Frank Niessink <frank@niessink.com>
2009-05-15 19:13:53 +0000

    Added chardet package to setup.py.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setupOptions = {
         'config', 'command', 'widgets', 'gui', 'gui.dialog', 'gui.viewer', 
         'i18n', 'patterns', 'mailer', 'help', 'domain', 'persistence', 
         'thirdparty', 'thirdparty.squaremap', 'thirdparty.lockfile', 
-        'syncml')] +
+        'thirdparty.chardet', 'syncml')] +
         ['taskcoachlib.domain.' + subpackage for subpackage in ('base',
         'date', 'category', 'effort', 'task', 'note', 'attachment')] +
         ['taskcoachlib.persistence.' + subpackage for subpackage in ('xml', 
