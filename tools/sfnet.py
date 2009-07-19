5d1fc675cde81ee5332fb1b83c588dc17797c3b2
Jérôme Laheurte <jerome@jeromelaheurte.net>
2009-07-19 12:14:47 +0000

    Add copyright information

--- tools/sfnet.py
+++ tools/sfnet.py
@@ -1,3 +1,20 @@
+#!/usr/bin/env python
+
+## Task Coach - Your friendly task manager
+## Copyright (C) 2009 Jerome Laheurte <fraca7@free.fr>
+
+## Task Coach is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+
+## Task Coach is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 
@@ -5,7 +22,8 @@ SF.net automation.
 
 When using IE8,
 
- * Enable 'Display mixed content' in the Misc section of the security settings for the Internet zone.
+ * Enable 'Display mixed content' in the Misc section of the security
+   settings for the Internet zone.
  * Disable flash! It makes IE crash on sourceforge!
 
 """
