09c104bfddf66dd646abb226d41183501efa5e28
Jérôme Laheurte <jerome@jeromelaheurte.net>
2009-08-02 14:25:55 +0000

    Forgot to remove the revision line...

--- taskcoachlib/meta/data.py
+++ taskcoachlib/meta/data.py
@@ -28,7 +28,6 @@ except ImportError:
 
 # The buildbot sets TCVERSION when building snapshots.
 version = os.environ.get('TCVERSION', '0.73.3')
-revision = '$Rev$'.split(' ')[1] # Subversion revision number
 tskversion = 24 # Current version number of the task file format
 release_day = '0' # Day number of the release, 1-31, as string
 release_month = 'July' # Month of the release in plain English 
