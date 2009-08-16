78adf8a8f88654d52e42cd8ad919bb09564bed2f
Jérôme Laheurte <jerome@jeromelaheurte.net>
2009-08-16 06:58:47 +0000

    Status is derived from database

--- taskcoach-iphone/Classes/TaskViewController.m
+++ taskcoach-iphone/Classes/TaskViewController.m
@@ -385,7 +385,7 @@
 	if (indexPath.section == 0)
 	{
 		Task *task = [[Task alloc] initWithId:-1 name:@"" status:STATUS_NEW taskCoachId:nil description:@""
-									startDate:[[DateUtils instance] stringFromDate:[NSDate date]] dueDate:nil completionDate:nil];
+									startDate:[[DateUtils instance] stringFromDate:[NSDate date]] dueDate:nil completionDate:nil dateStatus:TASKSTATUS_UNDEFINED];
 		isCreatingTask = YES;
 		TaskDetailsController *ctrl = [[TaskDetailsController alloc] initWithTask:task category:categoryId];
 		[self.navigationController pushViewController:ctrl animated:YES];
@@ -424,7 +424,7 @@
 - (IBAction)onAddTask:(UIBarButtonItem *)button
 {
 	Task *task = [[Task alloc] initWithId:-1 name:@"" status:STATUS_NEW taskCoachId:nil description:@""
-								startDate:[[DateUtils instance] stringFromDate:[NSDate date]] dueDate:nil completionDate:nil];
+								startDate:[[DateUtils instance] stringFromDate:[NSDate date]] dueDate:nil completionDate:nil dateStatus:TASKSTATUS_UNDEFINED];
 	isCreatingTask = YES;
 	TaskDetailsController *ctrl = [[TaskDetailsController alloc] initWithTask:task category:categoryId];
 	[self.navigationController pushViewController:ctrl animated:YES];
