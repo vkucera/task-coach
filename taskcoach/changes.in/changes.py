7874203cab5abb3baa0d47da9bb2020d8794872d
Frank Niessink <frank@niessink.com>
2009-10-25 21:40:49 +0000

    Task viewers now have buttons on the toolbar to hide or show completed and/or inactive (future) tasks (http://uservoice.com/a/EfkNL).

--- taskcoach/changes.in/changes.py
+++ taskcoach/changes.in/changes.py
@@ -23,6 +23,15 @@ from changetypes import *
 
 releases = [
             
+Release('0.76.0', 'October 31, 2009',
+    summary='''This release adds buttons on the toolbar of task viewers to
+hide completed and/or future tasks.''',
+    featuresAdded=[
+        Feature('''Task viewers now have buttons on the toolbar to hide or 
+show completed and/or inactive (future) tasks.''', 'http://uservoice.com/a/EfkNL')
+        ]
+    ),
+            
 Release('0.75.1', 'October 24, 2009',
     summary='''This is a bugfix release.''',
     bugsFixed=[
