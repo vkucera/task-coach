df7022c6da8570dd8a33edbdb4354519399b2207
Frank Niessink <frank@niessink.com>
2009-12-30 15:43:17 +0000

    Added Ctrl+E keyboard shortcut to "New effort..." menu item.

--- taskcoach/taskcoachlib/meta/data.py
+++ taskcoach/taskcoachlib/meta/data.py
@@ -29,7 +29,7 @@ except ImportError:
 # The buildbot sets TCVERSION when building snapshots.
 version = os.environ.get('TCVERSION', '0.78.0')
 tskversion = 28 # Current version number of the task file format
-release_day = '29' # Day number of the release, 1-31, as string
+release_day = '30' # Day number of the release, 1-31, as string
 release_month = 'December' # Month of the release in plain English 
 release_year = '2009' # Year of the release as string
 release_status = 'alpha' # One of 'alpha', 'beta', 'stable'
