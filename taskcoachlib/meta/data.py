4c5390d81c93726afed9f41aacc64567edda6ce3
Frank Niessink <frank@niessink.com>
2010-01-23 13:48:55 +0000

    Updated summary text about Task Coach.

--- taskcoachlib/meta/data.py
+++ taskcoachlib/meta/data.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
+
 '''
 Task Coach - Your friendly task manager
 Copyright (C) 2004-2010 Frank Niessink <frank@niessink.com>
-Copyright (C) 2008-2009 Jerome Laheurte <fraca7@free.fr>
+Copyright (C) 2008-2009 Jérôme Laheurte <fraca7@free.fr>
 
 Task Coach is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -53,6 +55,7 @@ long_description = '%(name)s is a simple open source todo manager to manage' \
 keywords = 'task manager, todo list, pim, time registration, track effort'
 author_first, author_last = 'Frank', 'Niessink' # Needed for PAD file
 author = '%s %s and Jerome Laheurte'%(author_first, author_last)
+author_unicode = u'%s %s and Jérôme Laheurte'%(author_first, author_last)
 author_email = 'developers@taskcoach.org'
 
 filename = name.replace(' ', '')
