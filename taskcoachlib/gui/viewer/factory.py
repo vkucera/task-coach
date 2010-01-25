37aa75299acdc99fc44a901e9bfd583bdcacca12
Frank Niessink <frank@niessink.com>
2010-01-25 22:06:35 +0000

    Add timelineviewer to list of viewer types.

--- taskcoachlib/gui/viewer/factory.py
+++ taskcoachlib/gui/viewer/factory.py
@@ -1,6 +1,6 @@
 '''
 Task Coach - Your friendly task manager
-Copyright (C) 2004-2009 Frank Niessink <frank@niessink.com>
+Copyright (C) 2004-2010 Frank Niessink <frank@niessink.com>
 
 Task Coach is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@ import effort, task, category, note
 
 
 def viewerTypes():
-    return 'squaretaskviewer', 'taskviewer', 'noteviewer', 'categoryviewer', 'effortviewer'
+    return 'timelineviewer', 'squaretaskviewer', 'taskviewer', 'noteviewer', 'categoryviewer', 'effortviewer'
 
 
 class addViewers(object):
