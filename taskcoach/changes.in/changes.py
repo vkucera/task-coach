d61f98849907141df98288ba8cba77f59a16e097
Frank Niessink <frank@niessink.com>
2010-04-19 21:44:06 +0000

    Remove static FAQ page and use Launchpad for FAQs. This makes it easier to add/change FAQs.

--- taskcoach/changes.in/changes.py
+++ taskcoach/changes.in/changes.py
@@ -23,7 +23,7 @@ from changetypes import *
 
 releases = [
 
-Release('1.0.3', 'April 18, 2010',
+Release('1.0.3', 'April 19, 2010',
     summary='''This release fixes a few bugs, and improves a few features, such
 as making the calendar view sortable.''', 
     bugsFixed=[
