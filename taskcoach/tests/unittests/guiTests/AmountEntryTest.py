7bb25b316adad9ada897ba70bac94fdccf0c648f
Frank Niessink <frank@niessink.com>
2010-12-31 15:37:16 +0000

    Update copyright periods to include 2011.

--- taskcoach/tests/unittests/guiTests/AmountEntryTest.py
+++ taskcoach/tests/unittests/guiTests/AmountEntryTest.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 '''
 Task Coach - Your friendly task manager
-Copyright (C) 2004-2010 Task Coach developers <developers@taskcoach.org>
+Copyright (C) 2004-2011 Task Coach developers <developers@taskcoach.org>
 
 Task Coach is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
