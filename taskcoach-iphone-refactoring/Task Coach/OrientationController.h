b748a770a1127c20e70aee622d7896a0ae71c4e7
Jérôme Laheurte <jerome@jeromelaheurte.net>
2011-04-03 10:36:06 +0000

    Smaller icons

---
+++ taskcoach-iphone-refactoring/Task Coach/OrientationController.h
@@ -0,0 +1,16 @@
+//
+//  OrientationController.h
+//  Task Coach
+//
+//  Created by Jérôme Laheurte on 03/04/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface OrientationController : UIViewController
+{
+}
+
+@end
---
+++ taskcoach-iphone-refactoring/Task Coach/OrientationController.m
@@ -0,0 +1,19 @@
+//
+//  OrientationController.m
+//  Task Coach
+//
+//  Created by Jérôme Laheurte on 03/04/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "OrientationController.h"
+
+
+@implementation OrientationController
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return YES;
+}
+
+@end
