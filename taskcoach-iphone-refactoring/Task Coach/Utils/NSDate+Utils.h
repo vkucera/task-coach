5771d921790bf385295e583c8c9534c1ed4ac933
Jérôme Laheurte <jerome@jeromelaheurte.net>
2011-04-04 15:48:29 +0000

    Add NSDate categories

---
+++ taskcoach-iphone-refactoring/Task Coach/Utils/NSDate+Utils.h
@@ -0,0 +1,17 @@
+//
+//  NSDate+Utils.h
+//  TaskCoach
+//
+//  Created by Jérôme Laheurte on 09/05/10.
+//  Copyright 2010 Jérôme Laheurte. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NSDate (Utils)
+
+// Current date, rounded to the minute start
++ (NSDate *)dateRounded;
+
+@end
