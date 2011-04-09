e66b1df86118778622c549a2b5edd3ae0d68f38d
Frank Niessink <frank@niessink.com>
2011-04-09 09:02:05 +0000

    Release date.

--- taskcoach/changes.in/changes.py
+++ taskcoach/changes.in/changes.py
@@ -22,7 +22,7 @@ from changetypes import *
 
 releases = [
 
-Release('1.2.13', 'April 7, 2011',
+Release('1.2.13', 'April 9, 2011',
     summary='''This is a mixed bugfix and feature release.''',
     bugsFixed=[
         Bug('''The notes section was missing from the help contents.''', 
