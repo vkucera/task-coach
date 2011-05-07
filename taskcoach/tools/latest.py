c1d1b0f80ceee917ed399b5d2a289471beaace6c
Jérôme Laheurte <jerome@jeromelaheurte.net>
2011-05-07 08:16:20 +0000

    Update CGI script

--- taskcoach/tools/latest.py
+++ taskcoach/tools/latest.py
@@ -1,27 +1,9 @@
 #!/usr/bin/python
 
-'''
-Task Coach - Your friendly task manager
-Copyright (C) 2004-2011 Task Coach developers <developers@taskcoach.org>
-
-Task Coach is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Task Coach is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
-
-import os, re
+import os, re, sys
 
 def findLatest(path, valid):
-    rx = re.compile('(.*)r(\d+)(.*)')
+    rx = re.compile(r'(.*)(\d+\.\d+\.\d+\.\d+)(.*)')
 
     results = dict()
 
@@ -30,14 +12,14 @@ def findLatest(path, valid):
             mt = rx.search(name)
             if mt:
                 ls = results.get((mt.group(1), mt.group(3)), [])
-                ls.append(int(mt.group(2)))
+                ls.append(map(int, mt.group(2).split('.')))
                 results[(mt.group(1), mt.group(3))] = ls
 
     packages = []
 
     for (part1, part3), versions in results.items():
         versions.sort()
-        packages.append('%sr%d%s' % (part1, versions[-1], part3))
+        packages.append('%s%s%s' % (part1, '.'.join(map(str, versions[-1])), part3))
 
     packages.sort()
 
@@ -52,7 +34,10 @@ def listPath(path):
     print '<ul>'
 
     for pkgname in findLatest(path, isSource):
-        print '<li><a href="http://www.fraca7.net/TaskCoach-packages/%s">%s</a></li>' % (pkgname, pkgname)
+        if path == '.' or path == 'all':
+            print '<li><a href="http://www.fraca7.net/TaskCoach-packages/%s">%s</a></li>' % (pkgname, pkgname)
+        else:
+            print '<li><a href="http://www.fraca7.net/TaskCoach-packages/%s/%s">%s</a></li>' % (path, pkgname, pkgname)
 
     print '</ul>'
 
@@ -60,29 +45,42 @@ def listPath(path):
     print '<ul>'
 
     for pkgname in findLatest(path, lambda x: not isSource(x)):
-        print '<li><a href="http://www.fraca7.net/TaskCoach-packages/%s">%s</a></li>' % (pkgname, pkgname)
+        if path == '.' or path == 'all':
+            print '<li><a href="http://www.fraca7.net/TaskCoach-packages/%s">%s</a></li>' % (pkgname, pkgname)
+        else:
+            print '<li><a href="http://www.fraca7.net/TaskCoach-packages/%s/%s">%s</a></li>' % (path, pkgname, pkgname)
 
     print '</ul>'
 
-def main():
+def main(path):
     print 'Content-type: text/html'
     print
 
     print '<html><head><title>Latest Task Coach builds</title>'
     print '<style type="text/css" media="screen">@import "default.css";</style>'
     print '</head></body>'
-    print '<h1>From Trunk</h1>'
-    listPath('.')
 
-    for name in os.listdir('branches'):
-        fname = os.path.join('branches', name)
-        if os.path.isdir(fname):
-            print '<h1>From %s</h1>' % name
-            listPath(fname)
+    if path == '.' or path == 'all':
+        print '<h1>New developments (from trunk)</h1>'
+        listPath('.')
+
+    if path != '.':
+        for name in os.listdir(path):
+            if name.startswith('Release'):
+                fname = os.path.join(path, name)
+                if os.path.isdir(fname):
+                    print '<h1>Bug fixes (from %s)</h1>' % name
+                    listPath(fname)
 
     print '<a href="http://www.taskcoach.org/download.html>Back to Task Coach downloads</a>'
 
     print '</body></html>'
 
 if __name__ == '__main__':
-    main()
+    if sys.argv[0].endswith('latest_features.py'):
+        main('.')
+    elif sys.argv[0].endswith('latest_bugfixes.py'):
+        main('branches')
+    else:
+        main('all')
+
