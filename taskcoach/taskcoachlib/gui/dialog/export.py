f9754d24b8795775c5da738cdaaa83a6b24ca66f
Frank Niessink <frank@niessink.com>
2011-05-16 21:00:48 +0000

    Client data for comboxes not working on Fedora, use a dict instead.

--- taskcoach/taskcoachlib/gui/dialog/export.py
+++ taskcoach/taskcoachlib/gui/dialog/export.py
@@ -47,8 +47,12 @@ class ExportDialog(sized_controls.SizedDialog):
         label = wx.StaticText(panel, label=('Export items from:'))
         sized_controls.SetSizerProp(label, 'valign', 'center')
         self.viewerComboBox = wx.ComboBox(panel, style=wx.CB_READONLY|wx.CB_SORT)
+        self.titleToViewer = dict()
         for viewer in self.exportableViewers():
-            self.viewerComboBox.Append(viewer.title(), viewer)
+            self.viewerComboBox.Append(viewer.title())
+            # Would like to user client data in the combobox, but that 
+            # doesn't work on all platforms
+            self.titleToViewer[viewer.title()] = viewer
         selectedViewer = self.window.viewer.activeViewer()
         if selectedViewer not in self.exportableViewers():
             selectedViewer = self.exportableViewers()[0]
@@ -69,7 +73,7 @@ class ExportDialog(sized_controls.SizedDialog):
         return self.selectionOnlyCheckBox.GetValue()
     
     def selectedViewer(self):
-        return self.viewerComboBox.GetClientData(self.viewerComboBox.GetSelection())
+        return self.titleToViewer[self.viewerComboBox.GetValue()]
 
     def onOK(self, event):
         self.settings.set(self.section, self.selectionOnlySetting, 
