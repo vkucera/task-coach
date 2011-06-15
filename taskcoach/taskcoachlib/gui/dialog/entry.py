e4bb1062c91ee7bb1f873dbd2eb8dd5b904c8244
Jérôme Laheurte <jerome@jeromelaheurte.net>
2011-06-15 10:55:54 +0000

    Add missing import

--- taskcoach/taskcoachlib/gui/dialog/entry.py
+++ taskcoach/taskcoachlib/gui/dialog/entry.py
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-import wx, locale
+import wx, wx.combo, locale
 from wx.lib import masked, newevent
 from taskcoachlib import widgets
 from taskcoachlib.gui import artprovider
