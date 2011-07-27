2b8527760c6f99ea5c4dafc9127c73b3c095f6cb
Frank Niessink <frank@niessink.com>
2011-07-27 20:25:09 +0000

    Merge from 1.3 branch.

--- taskcoach/taskcoachlib/domain/task/task.py
+++ taskcoach/taskcoachlib/domain/task/task.py
@@ -162,7 +162,7 @@ class Task(note.NoteOwner, attachment.AttachmentOwner,
     def setCategories(self, *categories, **kwargs):
         super(Task, self).setCategories(*categories, **kwargs)
         self.recomputeAppearance(True, event=kwargs.pop('event'))
-        
+                
     def allChildrenCompleted(self):
         ''' Return whether all children (non-recursively) are completed. '''
         children = self.children()
@@ -187,6 +187,7 @@ class Task(note.NoteOwner, attachment.AttachmentOwner,
             self.setDueDateTime(child.dueDateTime(), event=event)           
         if child.startDateTime() < self.startDateTime():
             self.setStartDateTime(child.startDateTime(), event=event)
+        child.recomputeAppearance(recursive=True, event=event)
 
     @patterns.eventSource
     def removeChild(self, child, event=None):
@@ -197,6 +198,7 @@ class Task(note.NoteOwner, attachment.AttachmentOwner,
         if self.shouldBeMarkedCompleted(): 
             # The removed child was the last uncompleted child
             self.setCompletionDateTime(date.Now(), event=event)
+        child.recomputeAppearance(recursive=True, event=event)
                     
     def childChangeEvent(self, child, event):
         childHasTimeSpent = child.timeSpent(recursive=True)
