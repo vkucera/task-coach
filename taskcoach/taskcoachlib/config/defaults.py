ef68aff45b6d9dd6fc7b0d159c1c262de5be68bf
Frank Niessink <frank@niessink.com>
2011-10-02 11:31:49 +0000

    Merged with release 1.2 branch.

--- taskcoach/taskcoachlib/config/defaults.py
+++ taskcoach/taskcoachlib/config/defaults.py
@@ -66,7 +66,7 @@ defaults = { \
     'searchdescription': 'False',
     'columns': "['startDateTime', 'dueDateTime']",
     'columnsalwaysvisible': "['subject']",
-    'columnwidths': "{'attachments': 28, 'notes': 28}",
+    'columnwidths': "{'attachments': 28, 'notes': 28, 'ordering': 38}",
     'columnautoresizing': 'True',
     'tasksdue': 'Never',             # Show tasks when they're due today, tomorrow, etc.
     'taskscompleted': 'Never',       # Show tasks completed today, yesterday, etc.
