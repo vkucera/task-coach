d8394c95ef8e3dd74069d61c6f0527fc6889dc09
Jérôme Laheurte <jerome@jeromelaheurte.net>
2012-02-03 13:14:16 +0000

    Fix iCalendar exporting.

--- taskcoach/changes.in/changes.py
+++ taskcoach/changes.in/changes.py
@@ -40,6 +40,8 @@ saving and opening the task file, the task would still have an actual start date
 (based on the earliest effort).''', '3478684', '3479444'),
         Bug('''Marking an inactive task active wouldn't properly update the 
 task icon from grey to blue.''', '3479444'),
+        Bug('''Exporting to iCalendar wouldn't work if the subject or
+description had non-ASCII characters.''', '3483124'),
         ],
     featuresRemoved=[
         Feature('''The more complex filtering options for hiding tasks have
