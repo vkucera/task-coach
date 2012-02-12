3c7a02583d7803ebe4d649b581217650372f7568
Frank Niessink <frank@niessink.com>
2012-02-12 19:46:43 +0000

    Try to force the paypal landing page to be in English and not in Dutch.

--- taskcoach/website.in/make.py
+++ taskcoach/website.in/make.py
@@ -221,6 +221,7 @@ pages['givesupport'] = '''
                         <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
                             <input type="hidden" name="cmd" value="_s-xclick">
                             <input type="hidden" name="hosted_button_id" value="FA3B7MEK8MXZG">
+                            <input type="hidden" name="lc" value="en">
                             <input style="width: 92px; height: 47px;" type="image" src="https://www.paypalobjects.com/en_US/NL/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
                             <img alt="" border="0" src="https://www.paypalobjects.com/nl_NL/i/scr/pixel.gif" width="1" height="1">
                         </form>
