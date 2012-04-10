9ec4dcdeb0cac2f1be17fd5aa08eb08b4f3bb637
Frank Niessink <frank@niessink.com>
2012-04-10 22:49:09 +0000

    Force build.

--- taskcoach/make.py
+++ taskcoach/make.py
@@ -16,7 +16,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-
 from taskcoachlib import meta
 import sys, os, glob, wx
 from setup import setupOptions
