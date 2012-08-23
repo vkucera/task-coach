bdb077f4bf5cb8836bcf1ab861dcbdc6a61e6db8
Frank Niessink <frank@niessink.com>
2012-08-23 20:37:07 +0000

    Add bug reference.

--- taskcoach/changes.in/changes.py
+++ taskcoach/changes.in/changes.py
@@ -60,7 +60,10 @@ visible columns in the effort tab and the notes tab) wouldn't be consistently
 applied to each edit dialog.''', '3559057'),
         Bug('''On Mac OS X, the button in the effort edit dialog for dropping
 down the tree of tasks didn't work.''', '3560296'),
-        Bug('''Make recurrence label clearer.''', '3560420'),
+        Bug('''Make recurrence label in the task edit dialog clearer.''', 
+            '3560420'),
+        Bug('''Make description of idle time notice in the preferences 
+clearer.''', '3555498'),
         Bug('''The combobox for selecting the snooze time in the reminder
 dialog was not read only. This suggested that one could type in custom snooze 
 times while that is not supported (yet).''', '3560416'),
