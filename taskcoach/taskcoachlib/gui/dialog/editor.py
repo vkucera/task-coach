df9bf3539ab9d7f6f4b4714da703926284978f02
Frank Niessink <frank@niessink.com>
2012-08-27 10:29:26 +0000

    When creating a new subitem, set focus to the subject field so that the description tab is raised if needed.

--- taskcoach/taskcoachlib/gui/dialog/editor.py
+++ taskcoach/taskcoachlib/gui/dialog/editor.py
@@ -1124,7 +1124,7 @@ class Editor(widgets.Dialog):
         self._callAfter = kwargs.get('callAfter', wx.CallAfter)
         super(Editor, self).__init__(parent, self.title(), 
                                      buttonTypes=wx.ID_CLOSE, *args, **kwargs)
-        columnName = kwargs.get('columnName', 'subject')
+        columnName = kwargs.get('columnName', 'subject') or 'subject'
         self._interior.setFocus(columnName)
         patterns.Publisher().registerObserver(self.onItemRemoved,
             eventType=container.removeItemEventType(), eventSource=container)
