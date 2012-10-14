bfbd6eecc973f365a1feb4bd71ff5439b6860bc8
Jérôme Laheurte <jerome@jeromelaheurte.net>
2012-10-14 10:24:01 +0000

    Try to keep the same layout as before.

--- taskcoach/taskcoachlib/thirdparty/__init__.py
+++ taskcoach/taskcoachlib/thirdparty/__init__.py
@@ -19,12 +19,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import desktop
 import sys, os
 sys.path.append(os.path.dirname(__file__))
-
-def _addPath(path):
-    sys.path.insert(0, os.path.join(os.path.dirname(__file__), path))
-
-_addPath('APScheduler-2.0.2')
 import apscheduler.scheduler
-
-_addPath('chardet-1.0.1')
-import chardet
