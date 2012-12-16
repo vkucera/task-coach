97da918eead3f14d17b6caa020b2d957eb333515
Jérôme Laheurte <jerome@jeromelaheurte.net>
2012-12-16 11:56:02 +0000

    Performance improvement

--- taskcoach/changes.in/changes.py
+++ taskcoach/changes.in/changes.py
@@ -35,6 +35,7 @@ nor Linux.''', '1285'),
         Bugv2('''Search defaults to simple substring matching.''', '1286'),
         Bugv2('''Growl notifications didn't work with Growl 2.0.''', '1324'),
         Bugv2('''Fix another encoding problem on some locales (Windows)'''),
+        Bugv2('''Performance improvement in the edit dialogs.'''),
         ],
     featuresAdded=[
         Feature('''Display dates in human-readable form in viewers (today,
