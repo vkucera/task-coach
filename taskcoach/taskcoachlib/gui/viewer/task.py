ea46a781faa7551d8cc3c4a12945897cbc5ad6b7
Jérôme Laheurte <jerome@jeromelaheurte.net>
2013-01-20 14:38:44 +0000

    Fix typo

--- taskcoach/taskcoachlib/gui/viewer/task.py
+++ taskcoach/taskcoachlib/gui/viewer/task.py
@@ -82,7 +82,7 @@ class BaseTaskViewer(mixin.SearchableViewerMixin,  # pylint: disable=W0223
             hasattr(wx.GetTopLevelParent(self), 'AddBalloonTip'):
             wx.GetTopLevelParent(self).AddBalloonTip(self.settings, 'filtershiftclick', self.toolbar,
                         getRect=lambda: self.toolbar.GetToolRect(self.toolbar.getToolIdByCommand('ViewerHideTasks_completed')),
-                        message=_('''Shift-click on a filter tool to see only tasks belonging to the correspondig status'''))
+                        message=_('''Shift-click on a filter tool to see only tasks belonging to the corresponding status'''))
 
     def __registerForAppearanceChanges(self):
         for appearance in ('font', 'fgcolor', 'bgcolor', 'icon'):
