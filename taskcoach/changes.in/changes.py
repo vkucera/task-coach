131b58f4f5f8f4db82ac20076599f8a248cabf38
Jérôme Laheurte <jerome@jeromelaheurte.net>
2013-03-23 19:25:14 +0000

    Fix date/time formatting on OS X.

--- taskcoach/changes.in/changes.py
+++ taskcoach/changes.in/changes.py
@@ -29,6 +29,8 @@ Release('1.3.28', 'March ??, 2013',
         Bugv2('''Fix task selection through keyboard in effort
 editor (MS Windows and GTK).''', '1400'),
         Bugv2('''Fix editing of effort start in the effort dialog.''', '1399'),
+        Bugv2('''Fix timezone-related display bug that would prevent
+the task editor from opening on OS X.''', '1395'),
         ],
     ),
 
