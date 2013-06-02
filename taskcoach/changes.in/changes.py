7a7a413308880ad1149e6557e431c6d47b65584c
Jérôme Laheurte <jerome@jeromelaheurte.net>
2013-06-02 12:08:43 +0000

    Fix ICC warning

--- taskcoach/changes.in/changes.py
+++ taskcoach/changes.in/changes.py
@@ -27,6 +27,7 @@ Release('1.3.30', 'April ??, 2013',
     bugsFixed=[
         Bugv2('''Working day start hour would be reset to 0 every time
 preferences are opened.''', '1418'),
+        Bugv2('''Fix the ICC warning with recent versions of libpng.''', '1422'),
         ],
     dependenciesChanged=[
         Dependency('''The minimal version of Python is back to 2.6.''')],
