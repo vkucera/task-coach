cd4aa51f3e993deda35f07ca86005c9f75a6d058
Jérôme Laheurte <jerome@jeromelaheurte.net>
2014-02-12 18:13:02 +0000

    Fix

--- taskcoach/uploadppa.sh
+++ taskcoach/uploadppa.sh
@@ -2,6 +2,10 @@
 
 for versionName in lucid precise quantal raring saucy; do
     for retryCount in 1 2 3 4 5 6 7 8 9 10; do
-	make ${1}_$versionName && break || exit 1
+	make ${1}_$versionName && break
     done
+    # Actually wrong, if wee succeeded on the 10th try...
+    if [ "$retryCount" = "10" ]; then
+	exit 1
+    fi
 done
