d4b6e7a04115a449c1def999e20dba065749bd31
Jérôme Laheurte <jerome@jeromelaheurte.net>
2014-02-12 19:05:48 +0000

    Merge with release 1.3 branch

--- taskcoach/uploadppa.sh
+++ taskcoach/uploadppa.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+make ppa_sign || exit 1
+
 for versionName in lucid precise quantal raring saucy; do
     for retryCount in 1 2 3 4 5 6 7 8 9 10; do
-	make ${1}_$versionName && break || exit 1
+	make ppa_${1}_$versionName && break
+	sleep 20
     done
+    # Actually wrong, if wee succeeded on the 10th try...
+    if [ "$retryCount" = "10" ]; then
+	exit 1
+    fi
 done
