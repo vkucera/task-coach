668b7c85a35f761a7c5b912153777da7d7fd0eca
Jérôme Laheurte <jerome@jeromelaheurte.net>
2014-05-25 09:26:22 +0000

    Set release date

--- taskcoach/changes.in/changes.py
+++ taskcoach/changes.in/changes.py
@@ -22,16 +22,18 @@ from changetypes import *
 
 releases = [
 
-Release('1.3.40', 'May ??, 2014',
-    summary='''This is a bugfix release.''',
+Release('1.3.40', 'May 25, 2014',
+    summary='''This is a mixed bugfix and feature release.''',
     bugsFixed=[
         Bugv2('''Remove a debug trace in DnD code.'''),
-        Bugv2('''Add an "add note to selected task" button in
-the task toolbar; patch from Manab Chetia'''),
         Bugv2('''"Effort for one task" becomes "Effort for
 selected task(s)"''', '1474', '1520'),
         Bugv2('''Fix an occasional "AlreadyCalledError" when iPhone
 sync is enabled.'''),
+        ],
+    featuresAdded=[
+        Feature('''Add an "add note to selected task" button in
+the task toolbar; patch from Manab Chetia'''),
         ],
     ),
 
