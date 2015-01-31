86882a2803dd62427ecad78b240f8f5f078d304e
Jérôme Laheurte <jerome@jeromelaheurte.net>
2015-01-31 14:22:31 +0000

    Update copyright notices

--- taskcoach/setup.py
+++ taskcoach/setup.py
@@ -2,7 +2,7 @@
 
 '''
 Task Coach - Your friendly task manager
-Copyright (C) 2004-2014 Task Coach developers <developers@taskcoach.org>
+Copyright (C) 2004-2015 Task Coach developers <developers@taskcoach.org>
 
 Task Coach is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
