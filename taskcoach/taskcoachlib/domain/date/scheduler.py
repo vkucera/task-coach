a3920137874da2954c2c11d3b0401d0a6d0f97d7
Jérôme Laheurte <fraca7@free.fr>
2020-02-24 18:26:37 +0100

    Remove duplicate import, contributed by Christopher Head

--- taskcoach/taskcoachlib/domain/date/scheduler.py
+++ taskcoach/taskcoachlib/domain/date/scheduler.py
@@ -19,7 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from taskcoachlib import patterns
 import dateandtime, timedelta
 import logging
-import timedelta
 import weakref
 import bisect
 
