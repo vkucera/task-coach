77e899dafd1f24cf99ecbc4d79cb82f51150ffd6
Jérôme Laheurte <fraca7@free.fr>
2020-09-05 14:58:15 +0200

    Update release date

--- taskcoach/taskcoachlib/meta/data.py
+++ taskcoach/taskcoachlib/meta/data.py
@@ -25,8 +25,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 version = '1.4.7'  # Current version number of the application
 tskversion = 37  # Current version number of the task file format, changed to 37 for release 1.3.23.
 release_day = '27'  # Day number of the release, 1-31, as string
-release_month = 'April'  # Month of the release in plain English
-release_year = '2019'  # Year of the release as string
+release_month = 'September'  # Month of the release in plain English
+release_year = '2020'  # Year of the release as string
 release_status = 'stable'  # One of 'alpha', 'beta', 'stable'
 
 # No editing needed below this line for doing a release.
