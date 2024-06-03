8e2fedd07bf8895674c11283302132f3d489591e
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-06-03 22:15:56 +0200

    Update developermessagechecker.py

--- taskcoach/taskcoachlib/meta/developermessagechecker.py
+++ taskcoach/taskcoachlib/meta/developermessagechecker.py
@@ -18,13 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from . import data
 import threading
-# selon le site https://diveintopython3.net/porting-code-to-python-3-with-2to3.html
-try:
-    import urllib2
-except ImportError:
-    import urllib.request as urllib2
-# mais il y a des conditions pour macOS
-# voir : https://docs.python.org/fr/3.11/library/urllib.request.html?highlight=urllib%20request#module-urllib.request
+import urllib.request as urllib2
     
 
 class DeveloperMessageChecker(threading.Thread):
